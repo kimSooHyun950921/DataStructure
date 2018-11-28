12088f923cb3fddeab020217387322cea7e4c853
kimSooHyun950921 <shkim950921@naver.com>
2018-11-28 23:37:56 +0900

    Update _Main_DS2P12_201402329_김수현.java

--- DS2P12_201402329/src/app/_Main_DS2P12_201402329_김수현.java
+++ DS2P12_201402329/src/app/_Main_DS2P12_201402329_김수현.java
@@ -1,10 +1,12 @@
 package app;
 
-public class _Main_DS2P12_201402329_±è¼öÇö {
+public class _Main_DS2P12_201402329_Â±Ã¨Â¼Ã¶Ã‡Ã¶ {
 
 	public static void main(String[] args) {
 		AppController appController = new AppController();
 		appController.run();
+		
+		
 	}
 
 }
